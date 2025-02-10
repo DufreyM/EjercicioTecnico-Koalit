5a6322fc85691889c9efa72fb225c04f25b7976d
Leonardo Mejía <leomejia646@gmail.com>
2025-02-10 12:27:09 -0600

    Creación de pantalla de Login y Pantalla de carga

---
+++ app/src/main/java/com/example/koalit_recetas/screens/Login.kt
@@ -0,0 +1,137 @@
+package com.example.koalit_recetas.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DinnerDining
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController? = null) {
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val rememberMe = remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFFFF9800), Color(0xFF807343))
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            // Icono de arriba, usuario
+            Icon(
+                imageVector = Icons.Default.DinnerDining,
+                contentDescription = "Icono Usuario",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .background(Color.White.copy(alpha = 0.3f))
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("El arte del sabor", color = Color.White, fontSize = 35.sp, fontFamily = FontFamily.Serif)
+
+            // Campos de entrada para iniciar la sesión.
+            // usuario y contraseña
+            OutlinedTextField(
+                value = email.value,
+                onValueChange = { email.value = it },
+                label = { Text("Usuario") },
+                leadingIcon = { Icon(Icons.Default.Person, contentDescription = "User") },
+                modifier = Modifier.fillMaxWidth(),
+
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = password.value,
+                onValueChange = { password.value = it },
+                label = { Text("Contraseña") },
+                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = "Lock") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth(),
+
+            )
+
+            // Olvidaste la contraseña
+            // No implementado
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = rememberMe.value,
+                        onCheckedChange = { rememberMe.value = it }
+                    )
+                    Text("Recuerdame", color = Color.White)
+                }
+                TextButton(onClick = { /* OLVIDASTE LA CONSTRASEÑA */ }) {
+                    Text("Olvidaste la contraseña", color = Color.White)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /* REDIRIGIR A LA PRINCIPAL */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(25.dp),
+                colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFD2800B), // Naranja
+                    contentColor = Color.White // Texto en blanco
+                )
+            ) {
+                Text("Iniciar Sesión", fontSize = 18.sp)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // No implementado
+            TextButton(onClick = { /* REDIRIGIR A LA PÁGINA DE REGISTRARSE */ }) {
+                Text("Registrarse", color = Color.White)
+            }
+        }
+    }
+}
+
+//Preview para probarlo
+@Preview(showBackground = true, backgroundColor = 0xFF6FE7B7)
+@Composable
+fun PreviewLoginScreen() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/example/koalit_recetas/screens/Pantalla_Carga.kt
@@ -0,0 +1,83 @@
+package com.example.koalit_recetas.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DinnerDining
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.SoupKitchen
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun Pantalla_Carga(navController: NavController? = null) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFFE79313), Color(0xFFAF9A4B))
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Icono de cocina
+            Icon(
+                imageVector = Icons.Default.SoupKitchen,
+                contentDescription = "Cocina",
+                tint = Color.White,
+                modifier = Modifier.size(150.dp) // Icono grande
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Texto El arte del sabor
+            Text(
+                text = "El arte del sabor",
+                color = Color.White,
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Serif
+            )
+        }
+
+        // "By: Koalit" en la esquina inferior derecha
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Text("By: Koalit", color = Color.White, fontSize = 14.sp)
+        }
+    }
+}
+
+// Preview para probarlo
+@Preview(showBackground = true, backgroundColor = 0xFF6FE7B7)
+@Composable
+fun PreviewPantalla_Carga() {
+    Pantalla_Carga()
+}
